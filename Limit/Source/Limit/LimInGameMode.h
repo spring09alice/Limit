a8636ae4e4da0eb3596f46bc616dc0418462296f
spring09alice <spring09alice@gmail.com>
2026-07-19 19:46:45 +0900

    build.cs

---
+++ Limit/Source/Limit/LimInGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LimInGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIMIT_API ALimInGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
